a40b76e65d2d9d65af1404ce6bc299ccb9bc5e32
estevamaraul07@gmail.com <estevamraul07@gmail.com>
2026-05-30 17:26:20 -0300

    adicionado model de nota fiscal com imports de cliente, vendedor e carro

---
+++ projeto/src/models/NotaFiscal.ts
@@ -0,0 +1,24 @@
+import { Cliente } from "./Cliente";
+import { Vendedor } from "./Vendedor";
+import { Carro } from "./Carro";
+
+export class NotaFiscal {
+    id_nota: number;
+    numero_nota: string;
+    data_emissao: Date;
+    valor_total: number;
+    id_cliente: Cliente;
+    id_vendedor: Vendedor;
+    id_carro: Carro;
+
+
+    constructor(id_cliente: Cliente, id_vendedor: Vendedor, id_carro: Carro, id_nota: number, numero_nota: string, data_emissao: Date, valor_total: number){
+        this.id_vendedor = id_vendedor;
+        this.id_carro = id_carro;
+        this.id_nota = Date.now();
+        this.id_cliente = id_cliente;
+        this.numero_nota = numero_nota;
+        this.data_emissao = data_emissao;
+        this.valor_total = valor_total;
+    }
+}
