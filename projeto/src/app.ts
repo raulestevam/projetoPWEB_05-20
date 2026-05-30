9d8e6a635deb97e6d6b96d25c13cb9c1623f6e08
estevamaraul07@gmail.com <estevamraul07@gmail.com>
2026-05-30 16:37:03 -0300

    adicionado código inicial das rotas do app.ts

--- projeto/src/app.ts
+++ projeto/src/app.ts
@@ -1,2 +1,8 @@
-let num:number
-let nn:number
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const PORT: number = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`API rodando na URL : http://localhost:${PORT}`));
