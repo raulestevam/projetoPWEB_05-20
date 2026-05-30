d053e73ec7d454b5d59c7c6d03ca9e3025bbf69a
estevamaraul07@gmail.com <estevamraul07@gmail.com>
2026-05-30 17:10:54 -0300

    adicionado model estoque com import de carro

---
+++ projeto/src/models/Estoque.ts
@@ -0,0 +1,18 @@
+import { Carro } from "./Carro";
+
+export class Estoque {
+    id_estoque: number;
+    id_carro: Carro;
+    quantidade: string;
+    localizacao_patio: string;
+    data_entrada: Date;
+
+
+    constructor( id_estoque: number, id_carro: Carro, quantidade: string, localizacao_patio: string, data_entrada: Date){
+        this.id_estoque = Date.now();
+        this.id_carro = id_carro;
+        this.quantidade = quantidade;
+        this.localizacao_patio = localizacao_patio;
+        this.data_entrada = data_entrada;
+    }
+}
