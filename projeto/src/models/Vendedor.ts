0cfc0de26c5ff214bdb6e2b933e5e85208482686
estevamaraul07@gmail.com <estevamraul07@gmail.com>
2026-05-30 17:15:40 -0300

    adicionado model Vendedor

---
+++ projeto/src/models/Vendedor.ts
@@ -0,0 +1,14 @@
+export class Vendedor {
+    id_vendedor: number;
+    nome: string;
+    matricula: string;
+    comissao_percentual: number;
+
+
+    constructor( id_vendedor: number, nome: string, matricula: string, comissao_percentual: number){
+        this.id_vendedor = Date.now();
+        this.nome = nome;
+        this.matricula = matricula;
+        this.comissao_percentual = comissao_percentual;
+    }
+}
