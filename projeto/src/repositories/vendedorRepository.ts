84ed6216ec73b2456847e171557cd50a6853989e
raulestevam <estevamrau07@gmail.com>
2026-05-30 22:29:23 -0300

    iniciado o repository de vendedor e comentado as funções que serão criadas

---
+++ projeto/src/repositories/vendedorRepository.ts
@@ -0,0 +1,10 @@
+import { Vendedor } from "../models/Vendedor";
+
+export class vendedorRepository{
+    // vendedor vazio
+    //inseir vendedor
+    //mostrar vendedores
+    //buscar vendedores
+    //atualizar vendedor
+    //deletar vendedor
+}
