34b84ca48584dd2aca9b59be0d2d6b594d984ebc
raulestevam <estevamrau07@gmail.com>
2026-05-31 22:37:54 -0300

    adicionado router do vendedor

---
+++ projeto/src/routers/vendedorRouter.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { vendedorController } from "../controllers/vendedorController";
+
+const router = Router();
+const controller = new vendedorController();
+
+router.get("/notas/:id", (req, res) => controller.listarNotasDoVendedor(req, res));
+ 
+router.get("/", (req, res) => controller.listarVendedores(req, res));
+router.get("/:id", (req, res) => controller.buscarVendedor(req, res));
+router.post("/", (req, res) => controller.criarVendedor(req, res));
+router.put("/:id", (req, res) => controller.atualizarVendedor(req, res));
+router.delete("/:id", (req, res) => controller.deletarVendedor(req, res));
+ 
+export default router;
