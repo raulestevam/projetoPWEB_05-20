e891f01876c19e9912621287d2bc17a16dfeedbd
raulestevam <estevamrau07@gmail.com>
2026-05-31 23:37:26 -0300

    teste de request com sucesso

--- projeto/src/app.ts
+++ projeto/src/app.ts
@@ -1,20 +1,20 @@
 import express, { Application, Request, Response } from "express";
 
-//import { listarVendedores, buscarVendedor, criarVendedor, atualizarVendedor, deletarVendedor, listarNotasDoVendedor } from "./controllers/vendedorController";
-//import { cadastrarCliente, exibirClientes, consultarCliente, modificarCliente, removerCliente } from "./controllers/clienteController";
+import { listarVendedores, buscarVendedor, criarVendedor, atualizarVendedor, deletarVendedor, listarNotasDoVendedor } from "./controllers/vendedorController";
+import { cadastrarCliente, exibirClientes, consultarCliente, modificarCliente, removerCliente } from "./controllers/clienteController";
 import { cadastrarCarro, atualizarCarro, listarTodos, buscarPorID, removerCarro } from "./controllers/carroController";
-//import { cadastrarEstoque, buscarEstoqueCarro, buscarEstoquePorId, listarEstoques, atualizarEstoque, deletarEstoque } from "./controllers/estoqueController";
+import { cadastrarEstoque, buscarEstoqueCarro, buscarEstoquePorId, listarEstoques, atualizarEstoque, deletarEstoque } from "./controllers/estoqueController";
 
 const app: Application = express();
 const PORT: number = 3000;
 app.use(express.json());
 
 //REQUESTS VENDEDORES
-/*app.get("/vendedores", listarVendedores);
+app.get("/vendedores", listarVendedores);
 app.get("/vendedores/:id", buscarVendedor);
 app.post("/vendedores", criarVendedor);
 app.put("/vendedores/:id", atualizarVendedor);
-app.delete("/vendedores/:id", deletarVendedor);*/
+app.delete("/vendedores/:id", deletarVendedor);
 
 //REQUESTS CARRO
 app.get("/carros", listarTodos);
@@ -24,19 +24,19 @@ app.put("/carros/:id", atualizarCarro);
 app.delete("/carros/:id", removerCarro);
 
 //REQUESTS CLIENTE
-// app.get("/clientes", exibirClientes);
-// app.get("/clientes/:id", consultarCliente);
-// app.post("/clientes", cadastrarCliente);
-// app.put("/clientes/:id", modificarCliente);
-// app.delete("/clientes/:id", removerCliente);
+app.get("/clientes", exibirClientes);
+app.get("/clientes/:id", consultarCliente);
+app.post("/clientes", cadastrarCliente);
+app.put("/clientes/:id", modificarCliente);
+app.delete("/clientes/:id", removerCliente);
 
 //REQUESTS ESTOQUE
-/*app.get("/estoque", listarEstoques);
+app.get("/estoque", listarEstoques);
 app.get("/estoque/carro/:id_carro", buscarEstoqueCarro);
 app.get("/estoque/:id", buscarEstoquePorId)
 app.post("/estoque", cadastrarEstoque);
 app.put("/estoque/:id", atualizarEstoque);
-app.delete("/estoque/:id", deletarEstoque);*/
+app.delete("/estoque/:id", deletarEstoque);
 
 //REQUESTS NOTA FISCAL
 
