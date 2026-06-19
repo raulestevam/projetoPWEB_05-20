65d92e1d3588eb542b5b326380d88a52236eed4d
raulestevam <estevamraul07@gmail.com>
2026-06-19 19:16:10 -0300

    atualizado app.ts para adicição de função assincrona e banco de dados mysql

--- projeto/src/app.ts
+++ projeto/src/app.ts
@@ -1,52 +1,18 @@
-import express, { Application, Request, Response } from "express";
-
-import { listarVendedores, buscarVendedor, criarVendedor, atualizarVendedor, deletarVendedor } from "./controllers/vendedorController";
-import { cadastrarCliente, exibirClientes, consultarCliente, modificarCliente, removerCliente } from "./controllers/clienteController";
-import { cadastrarCarro, atualizarCarro, listarTodos, buscarPorID, removerCarro, listarCarrosComEstoque } from "./controllers/carroController";
-import { cadastrarEstoque, buscarEstoqueCarro, buscarEstoquePorId, listarEstoques, atualizarEstoque, deletarEstoque } from "./controllers/estoqueController";
-import { listarNotas, buscarId, emitir, listarNotasPorCliente, listarNotasPorVendedor } from "./controllers/notaFiscalController";
+import express from "express"
+import router from "./routes/router";
+import { inicializarBanco } from "./database/mysql";
 
 const app: Application = express();
 const PORT: number = 3000;
 app.use(express.json());
 
-//REQUESTS VENDEDORES
-app.get("/vendedores", listarVendedores);
-app.get("/vendedores/:id", buscarVendedor);
-app.post("/vendedores", criarVendedor);
-app.put("/vendedores/:id", atualizarVendedor);
-app.delete("/vendedores/:id", deletarVendedor);
-app.get("/vendedores/notas/:id", listarNotasPorVendedor);
+app.use('/api', router);
 
-//REQUESTS CARRO
-app.get("/carros", listarTodos);
-app.get("/carros/disponiveis", listarCarrosComEstoque);
-app.get("/carros/:id", buscarPorID);
-app.post("/carros", cadastrarCarro);
-app.put("/carros/:id", atualizarCarro);
-app.delete("/carros/:id", removerCarro);
+async function startServer() {
+    await inicializarBanco();
+    app.listen(PORT, () => console.log(`API rodando na URL : http://localhost:${PORT}`));
+}
 
-//REQUESTS CLIENTE
-app.get("/clientes", exibirClientes);
-app.get("/clientes/:id", consultarCliente);
-app.post("/clientes", cadastrarCliente);
-app.put("/clientes/:id", modificarCliente);
-app.delete("/clientes/:id", removerCliente);
-app.get("/clientes/notas/:id", listarNotasPorCliente);
-
-//REQUESTS ESTOQUE
-app.get("/estoque", listarEstoques);
-app.get("/estoque/carro/:id_carro", buscarEstoqueCarro);
-app.get("/estoque/:id", buscarEstoquePorId);
-app.post("/estoque", cadastrarEstoque);
-app.put("/estoque/:id", atualizarEstoque);
-app.delete("/estoque/:id", deletarEstoque);
-
-//REQUESTS NOTA FISCAL
-app.get("/notas", listarNotas);
-app.get("/notas/:id", buscarId);
-app.post("/notas", emitir);
-
-app.listen(PORT, () => console.log(`API rodando na URL : http://localhost:${PORT}`));
+startServer();
 
 export default app;
