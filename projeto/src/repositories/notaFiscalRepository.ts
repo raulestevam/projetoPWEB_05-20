7c9f552a332e3ed496caee59fe53bbf91e1e7e1c
raulestevam <estevamrau07@gmail.com>
2026-06-28 22:16:40 -0300

    criado função assíncrona e comandos sql para a notaFiscal

--- projeto/src/repositories/notaFiscalRepository.ts
+++ projeto/src/repositories/notaFiscalRepository.ts
@@ -1,56 +1,74 @@
 import { NotaFiscal } from "../models/NotaFiscal";
+import { executarComandoSQL } from "../database/mysql";
 
 export class notaFiscalRepository {
     private static instance: notaFiscalRepository;
-    private notas: NotaFiscal[] = [];
 
     static getCreateTableQuery(): string {
-    return `
-        CREATE TABLE IF NOT EXISTS NotaFiscal (
-            id_nota         BIGINT PRIMARY KEY,
-            numero_nota     VARCHAR(50) NOT NULL UNIQUE,
-            data_emissao    DATETIME NOT NULL,
-            valor_total     DECIMAL(10,2) NOT NULL,
-            id_cliente      BIGINT NOT NULL,
-            id_vendedor     BIGINT NOT NULL,
-            id_carro        BIGINT NOT NULL,
-            FOREIGN KEY (id_cliente)  REFERENCES Cliente(id_cliente),
-            FOREIGN KEY (id_vendedor) REFERENCES Vendedor(id_vendedor),
-            FOREIGN KEY (id_carro)    REFERENCES Carro(id_carro)
-        );
-    `;
-}
-    
+        return `
+            CREATE TABLE IF NOT EXISTS NotaFiscal (
+                id_nota         BIGINT PRIMARY KEY,
+                numero_nota     VARCHAR(50) NOT NULL UNIQUE,
+                data_emissao    DATETIME NOT NULL,
+                valor_total     DECIMAL(10,2) NOT NULL,
+                id_cliente      BIGINT NOT NULL,
+                id_vendedor     BIGINT NOT NULL,
+                id_carro        BIGINT NOT NULL,
+                FOREIGN KEY (id_cliente)  REFERENCES Cliente(id_cliente),
+                FOREIGN KEY (id_vendedor) REFERENCES Vendedor(id_vendedor),
+                FOREIGN KEY (id_carro)    REFERENCES Carro(id_carro)
+            );
+        `;
+    }
+
     private constructor() {}
-    
+
     public static getInstance(): notaFiscalRepository {
-        if(!this.instance){
+        if (!this.instance) {
             this.instance = new notaFiscalRepository();
         }
         return this.instance;
     }
 
-    listarNotas(): NotaFiscal[] {
-        return this.notas;
-    }
-    
-    buscarNotaPorId(id: number): NotaFiscal | undefined {
-        return this.notas.find(n => n.id_nota === id);
+    async listarNotas(): Promise<NotaFiscal[]> {
+        const query = `SELECT * FROM NotaFiscal`;
+        return await executarComandoSQL(query, []);
     }
 
-    criarNota(nota: NotaFiscal) {
-        this.notas.push(nota);
+    async buscarNotaPorId(id: number): Promise<NotaFiscal | undefined> {
+        const query = `SELECT * FROM NotaFiscal WHERE id_nota = ?`;
+        const resultado = await executarComandoSQL(query, [id]);
+        return resultado[0];
     }
 
-    buscarPorCarroId(id_carro: number) {
-        return this.notas.filter(nota => nota.id_carro === id_carro);
+    async criarNota(nota: NotaFiscal): Promise<void> {
+        const query = `
+            INSERT INTO NotaFiscal (id_nota, numero_nota, data_emissao, valor_total, id_cliente, id_vendedor, id_carro)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        `;
+        await executarComandoSQL(query, [
+            nota.id_nota,
+            nota.numero_nota,
+            nota.data_emissao,
+            nota.valor_total,
+            nota.id_cliente,
+            nota.id_vendedor,
+            nota.id_carro,
+        ]);
     }
 
-    buscarPorClienteId(id_cliente: number) {
-        return this.notas.filter(nota => nota.id_cliente === id_cliente);
+    async buscarPorCarroId(id_carro: number): Promise<NotaFiscal[]> {
+        const query = `SELECT * FROM NotaFiscal WHERE id_carro = ?`;
+        return await executarComandoSQL(query, [id_carro]);
     }
 
-    buscarPorVendedorId(id_vendedor: number) {
-        return this.notas.filter(nota => nota.id_vendedor === id_vendedor);
+    async buscarPorClienteId(id_cliente: number): Promise<NotaFiscal[]> {
+        const query = `SELECT * FROM NotaFiscal WHERE id_cliente = ?`;
+        return await executarComandoSQL(query, [id_cliente]);
+    }
+
+    async buscarPorVendedorId(id_vendedor: number): Promise<NotaFiscal[]> {
+        const query = `SELECT * FROM NotaFiscal WHERE id_vendedor = ?`;
+        return await executarComandoSQL(query, [id_vendedor]);
     }
 }
